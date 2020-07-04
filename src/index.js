95f81209abde8046671b020fad6be936fdafe252
Arthur Fusco <fuscoarthur1@gmail.com>
2020-07-04 01:35:14 -0300

    Projeto upado no repositório

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Items(props) {
+    return (
+        <div className={props.className}>
+            {props.items}
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    const [items, setItems] = React.useState([]);
+    const [currentItem, setCurrentItem] = React.useState({
+        text: '',
+        key: ''
+    })
+
+    const handleCahnge = event => {
+        setCurrentItem({
+            text: event.target.value,
+            key: ''
+        })
+    }
+
+    const handleClick = () => {
+        if(currentItem.text) {
+            setItems([...items, currentItem]);
+            setCurrentItem({
+                text: currentItem.text,
+                key: Date.now()
+            })
+        }
+    }
+
+    const deleteItem = key => {
+        const filteredArr = items.filter(item => item.key !== key);
+        setItems(filteredArr)
+    }
+
+    const itemsList = items.map(item => (
+        <div key={item.key}>
+            {item.text}
+            <button onClick={() => deleteItem(item.key)}>Deletar</button>
+        </div>
+    ))
+
+    return (
+        <div className='container'>
+            <div className='container-input'>
+                <input className='input' value={currentItem.text} onChange={handleCahnge}></input>
+                <button className='add' onClick={handleClick}>Adicionar</button>
+            </div>
+
+            <div className='items-container'>
+                <Items className='items' items={itemsList} />
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
